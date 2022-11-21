a6a0cc6bfaad18368c14b4bd2424eb8d711c0c20
Isaa <daze.isaa@gmail.com>
2022-11-21 14:53:02 -0800

    carousel in home

--- src/app/Components/z0-home/z0-home.component.ts
+++ src/app/Components/z0-home/z0-home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'z0-home',
   templateUrl: './z0-home.component.html',
   styleUrls: ['./z0-home.component.css']
 })
-export class Z0HomeComponent implements OnInit {
+export class Z0HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems, {
+        indicators: true,
+        fullWidth: true
+      });
+    });
+
+  }
+  ngAfterViewInit(){
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems, {
+        indicators: true,
+        fullWidth: true
+      });
+    });
   }
 
 }
